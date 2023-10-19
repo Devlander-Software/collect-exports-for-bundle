1f2812f50e9a2d83b724277a2b4aff721aab1713
Landon Johnson <landonwjohnson@gmail.com>
2023-10-18 20:23:30 -0600

    remove dist

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// File:  index.js (JavaScript)
-// GitHub Gist URL: https://gist.github.com/landonwjohnson/6c444d07e8686711347fa474dd5540f5
-// Location: external-modules/auto-exporter-script/index.js
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.autoExporter = void 0;
-var auto_exporter_1 = require("./auto-exporter");
-Object.defineProperty(exports, "autoExporter", { enumerable: true, get: function () { return auto_exporter_1.autoExporter; } });
-//# sourceMappingURL=index.js.map
