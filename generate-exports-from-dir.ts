65f96aee05e43369e4cdec07043831be159b28bb
Landon Johnson <landonwjohnson@gmail.com>
2023-10-18 22:00:31 -0600

    ready

--- generate-exports-from-dir.ts
+++ generate-exports-from-dir.ts
@@ -1,10 +1,12 @@
 import * as fs from 'fs';
 import path from 'path';
+import { green, red, yellow } from 'picocolors';
 import { collectPaths } from './collect-paths';
 import { fileHasValidExtension } from './has-valid-extension';
 import { AutoExporterOptions } from "./types";
 
 export function generateExportsFromDir(startPath: string, config: AutoExporterOptions): string[] {
+    console.log(green('Starting export generation from directory...'));
     const collectedPaths: string[] = collectPaths(startPath, config);
     const distinctPaths = [...new Set(collectedPaths)]; // Remove duplicates using Set
     return generateExportsFromPaths(distinctPaths, config);
@@ -19,12 +21,14 @@ function hasDefaultExport(fileContent: string): boolean {
 }
 
 export function extractDefaultExportVariable(filepath: string): string | null {
+    console.log(green(`Extracting default export from ${filepath}...`));
     const fileContent = fs.readFileSync(filepath, 'utf-8');
     const defaultExportMatch = fileContent.match(/export default (\w+)/);
     return defaultExportMatch ? defaultExportMatch[1] : null;
 }
 
 export function generateExportsFromPaths(paths: string[], config: AutoExporterOptions): string[] {
+    console.log(green('Generating exports from provided paths...'));
     let results: string[] = [];
     let defaultExportString: string[] = [];
 
@@ -35,13 +39,14 @@ export function generateExportsFromPaths(paths: string[], config: AutoExporterOp
         const componentName = path.basename(filename, path.extname(filename));
 
         if (config.files && config.files.includes(filename)) {
+            console.log(yellow(`Processing included file: ${filename}...`));
             if (filename.endsWith(config.defaultExportFile || '') && hasDefaultExport(fileContent)) {
                 const defaultVariable = extractDefaultExportVariable(filename);
                 if (defaultVariable) {
                     defaultExportString.push(`import ${defaultVariable} from "${withoutExtension}";`);
                     defaultExportString.push(`export default ${defaultVariable};`);
                 } else {
-                    console.error(`Failed to extract default export from ${filename}.`);
+                    console.error(red(`Failed to extract default export from ${filename}.`));
                 }
             } else if (hasNamedExports(fileContent)) {
                 results.push(`/**\n * TSDoc for ${componentName}\n */`);
