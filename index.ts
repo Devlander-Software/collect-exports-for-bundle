79d60261b24cb250ceb8f1f3cf007371d0fefa02
Landon Johnson <landonwjohnson@gmail.com>
2023-10-20 16:08:25 -0600

    this is it

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // File:  index.js (JavaScript)
 // GitHub Gist URL: https://gist.github.com/landonwjohnson/6c444d07e8686711347fa474dd5540f5
 // Location: external-modules/auto-exporter-script/index.js
-import { autoExporter } from './auto-exporter';
+const autoExporter = require('./auto-exporter');
 export * from './collect-paths';
 export * from './color-log';
 export * from './generate-exports-from-dir';
