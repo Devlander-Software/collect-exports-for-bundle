f292ef678fd82747651c4ab57b68f55cee56b7e1
landonwjohnson <landonwjohnson@gmail.com>
2023-11-14 14:44:06 -0700

    exporting types from index file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+export type * from './types/module-exporter.types'
 export { default } from './utils/auto-exporter'
 export * from './utils/collect-paths'
 export * from './utils/collect-paths-from-directories'
 export * from './utils/create-extensions'
 export * from './utils/export-patterns'
+
